0c0bfc2cb0d17fb983e6dcc579b8501617291e96
daniela989898 <dandiazgonzalez@gmail.com>
2025-08-27 09:30:55 -0400

    Add files via upload

---
+++ y = 3.py
@@ -0,0 +1,16 @@
+y = 3
+x = 7 + y
+print(x)
+greeting = "hello, daniela"
+print(greeting.capitalize(20, '^'))
+print = "100"
+print(greeting.isdecimal())
+temperature = 85
+if temperature > 75:
+    print("It's hot outside!")
+    print("I need some water.")
+print("It's a great day.")
+if temperature == 100:
+    print("It's hot outside!")
+    print("I need some water.")
+    
